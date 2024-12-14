2d8fbdbba1f12309d9eaadc627e1f921cf9999f4
Andrei Valdez <andreivaldezau@gmail.com>
2024-12-15 00:24:47 +1100

    Update bubble and selection sort

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -1,11 +1,15 @@
-def bubble(array: list):
+def bubble(array: list) -> list:
     """Sort a list using bubble sort.
 
     Args:
-        array (list): unsorted array
+        array (list): unsorted list
+
+    Returns:
+        list: sorted list
     """
-    n = len(array)
+    n: int = len(array)
     for i in range(n - 1):
         for j in range(n - i - 1):
             if array[j] > array[j + 1]:
                 array[j], array[j + 1] = array[j + 1], array[j]
+    return array
--- algorithms/selection_sort.py
+++ algorithms/selection_sort.py
@@ -1,14 +1,18 @@
-def selection(array: list):
+def selection(array: list) -> list:
     """Sort a list using selection sort.
 
     Args:
-        array (list): unsorted array
+        array (list): unsorted list
+
+    Returns:
+        list: sorted list
     """
-    n = len(array)
+    n: int = len(array)
     for i in range(n - 1):
-        i_min = i
+        i_min: int = i
         for j in range(i + 1, n):
             if array[j] < array[i_min]:
                 i_min = j
-        value = array.pop(i_min)
+        value: int = array.pop(i_min)
         array.insert(i, value)
+    return array
