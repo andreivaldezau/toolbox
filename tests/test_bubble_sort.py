ba1399afddfe9c6df12cbd1d4d7f5cbfe14c0026
Andrei Valdez <andreivaldezau@gmail.com>
2025-01-01 14:29:58 +1100

    Update bubble sort tests

--- tests/test_bubble_sort.py
+++ tests/test_bubble_sort.py
@@ -9,22 +9,25 @@ def test_bubble_simple():
 
 
 def test_bubble_odd():
-    assert bubble([1, 2, 3]) == [1, 2, 3]
-    assert bubble([1, 3, 2]) == [1, 2, 3]
-    assert bubble([2, 1, 3]) == [1, 2, 3]
-    assert bubble([2, 3, 1]) == [1, 2, 3]
-    assert bubble([3, 1, 2]) == [1, 2, 3]
-    assert bubble([3, 2, 1]) == [1, 2, 3]
+    odd_array: list[int] = [1, 2, 3]
+    assert bubble([1, 2, 3]) == odd_array
+    assert bubble([1, 3, 2]) == odd_array
+    assert bubble([2, 1, 3]) == odd_array
+    assert bubble([2, 3, 1]) == odd_array
+    assert bubble([3, 1, 2]) == odd_array
+    assert bubble([3, 2, 1]) == odd_array
 
 
 def test_bubble_even():
-    assert bubble([1, 2, 3, 4]) == [1, 2, 3, 4]
-    assert bubble([4, 3, 2, 1]) == [1, 2, 3, 4]
+    even_array: list[int] = [1, 2, 3, 4]
+    assert bubble([1, 2, 3, 4]) == even_array
+    assert bubble([4, 3, 2, 1]) == even_array
 
 
 def test_bubble_negative():
-    assert bubble([-3, -2, -1]) == [-3, -2, -1]
-    assert bubble([-1, -2, -3]) == [-3, -2, -1]
+    negative_array: list[int] = [-3, -2, -1]
+    assert bubble([-3, -2, -1]) == negative_array
+    assert bubble([-1, -2, -3]) == negative_array
 
 
 def test_bubble_mixed():
